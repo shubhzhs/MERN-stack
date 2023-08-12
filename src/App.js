e57f0d56ca78734ebf84ae59c65485fdaf7719d1
shubhzhs <shubham1003683@gmail.com>
2023-08-12 11:46:09 +0530

    patching

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 import "./App.css";
-import "./components/GoalList";
-import GoalList from "./components/GoalList";
+import "./components/GoalList/GoalList";
+import GoalList from "./components/GoalList/GoalList";
 
 const App = () => {
   const courseGoals = [
     { id: "cg1", text: "Finish the course" },
-    { id: "cg1", text: "Learn all abot the course" },
-    { id: "cg1", text: "Help other students in the course Q&A" },
+    { id: "cg2", text: "Learn all about the course" },
+    { id: "cg3", text: "Help other students in the course Q&A" },
   ];
 
   return (
     <div className="goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
--- src/components/GoalList/GoalList.js
+++ src/components/GoalList/GoalList.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import "./goalList.css";
 
-const GoalList = () => {
+const GoalList = (props) => {
+  console.log(props.goals);
   return (
     <div>
       <ul className="goal-list">
-        <li>Finish the course</li>
-        <li>Learn all abot the course</li>
-        <li>Help other students in the course Q&A</li>
+        {props.goals.map((goal) => {
+          return <li key={goal.id}>{goal.text}</li>;
+        })}
       </ul>
     </div>
   );
